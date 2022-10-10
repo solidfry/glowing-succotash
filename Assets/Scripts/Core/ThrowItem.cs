12dd3f0e6d370c3b929bc8bd370ee4fe2ab7065a
solidfry <ellioteweb@gmail.com>
2022-10-10 20:18:24 +1100

    added better lighting and normals to goblin

---
+++ Assets/Scripts/Core/ThrowItem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Core
+{
+    public class ThrowItem : MonoBehaviour
+    {
+        [SerializeField] GameObject prefabToThrow;
+        [SerializeField] float throwDistance;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
